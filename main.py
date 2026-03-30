d28b883a8ee149b1839a3f7cfd2bc1f21ec7aedc
Aishwarya Shanmukh <aishwarya153009@gmail.com>
2026-03-30 10:33:13 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
+from resume_parser import parse_resume
+from feature_extraction import extract_skills
+from ranking_model import rank_candidate
 
+resume = "sample_resume.txt"
+
+text = parse_resume(resume)
+skills = extract_skills(text)
+score = rank_candidate(skills)
+
+print("Candidate Score:", score)
